e64a6877b0b8569dec66f4cd40e8f4f48af7ed14
Danny Nguyen <ndanny@github.com>
2024-09-10 21:49:33 -0700

    Add FAQ section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,50 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Danny was here
+    <div className="min-h-screen p-8 pb-20 sm:p-20">
+      <div className="space-y-5">
+        <h1 className="text-2xl">
+          Southern California Edison Energy Usage Parser
+        </h1>
+        <div className="max-w-screen-md">
+          <p className="font-bold">Why this tool?</p>
+          <p>
+            <Link
+              href="https://www.sce.com"
+              className="text-blue-500 font-bold"
+            >
+              Southern California Edison
+            </Link>{" "}
+            allows customers to download their energy usage data in CSV format.
+            This tool helps you parse that data and visualize it in a more
+            user-friendly way than the SCE website.
+          </p>
+        </div>
+        <div className="max-w-screen-md">
+          <p className="font-bold">How can I get my usage data?</p>
+          <p>
+            Visit{" "}
+            <Link
+              href="https://www.sce.com/sma/ESCAA/EscGreenButtonData"
+              className="text-blue-500 font-bold"
+            >
+              this link
+            </Link>{" "}
+            and log in to your SCE account. Select the date range you want to
+            see your energy usage for, choose the CSV format option, and
+            download the CSV. You will upload this CSV file to this tool.
+          </p>
+        </div>
+        <div className="max-w-screen-md">
+          <p className="font-bold">A note on privacy and security</p>
+          <p>
+            This tool only stores and processes your data in your browser. Your
+            data is never sent to any server. You can view the source code for
+            this tool on our GitHub.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
