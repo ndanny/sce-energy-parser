ea1fe24cf74c656ba487b2b72a228598448b361d
Danny Nguyen <ndanny@github.com>
2024-09-14 22:12:04 -0700

    Refactor header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ import FileUpload from "../components/feature/file_upload";
 export default function Home() {
   return (
     <div className="min-h-screen p-8 pb-20 sm:p-20">
-      <h1 className="text-2xl mb-6">
-        Southern California Edison Energy Usage Parser
-      </h1>
       {/* <Faq /> */}
       <FileUpload />
     </div>
---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <div className="p-6 bg-slate-100">
+      <p className="text-sm font-bold text-center md:text-left">⚡️ Southern California Edison Energy Usage Tool</p>
+    </div>
+  );
+};
