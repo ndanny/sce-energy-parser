5898b0c85552268ed674e440f91295bc5bcb26d8
Danny Nguyen <ndanny@github.com>
2024-09-15 12:12:08 -0700

    Update header and add GitHub link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,15 @@
+import { GitHubLogoIcon } from '@radix-ui/react-icons';
+import Link from 'next/link';
+
 export const Header = () => {
   return (
-    <div className="p-6 bg-slate-100">
-      <p className="text-sm font-bold text-center md:text-left">⚡️ Southern California Edison Energy Usage Tool</p>
+    <div className="p-6 border-b-2 flex flex-col md:flex-row items-center justify-between">
+      <p className="text-sm font-bold text-center md:text-left">
+        ⚡️ Southern California Edison Energy Usage Tool
+      </p>
+      <Link href="https://github.com/ndanny/sce-energy-parser" target="_blank" rel="noopener noreferrer" className="mt-4 md:mt-0">
+        <GitHubLogoIcon className="w-6 h-6 text-gray-600 hover:text-black" />
+      </Link>
     </div>
   );
 };
